df1ac8099be090e9e7c0cb681cada96ae2e474da
sorti777 <sorti@guardiaschool.fr>
2025-11-19 17:51:16 +0100

    Create pageSO.php

---
+++ page2/pageSO.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../main/style.css">
+    <title>Portfolio</title>
+</head>
+
+<body>
+    <header class="bandeau">
+        <h1>Simon Orti</h1>
+    </header>
+
+    <img src="../image/SimonO.jpg" alt="Photo" width="300">
+   <p class="nom">Simon Orti</p>
+   <p class="age">18 ans</p>
+   <p class="pérsonnalité">Rigoureux, Visionnaire, Analytique, Collaboratif</p>
+   <p class="compétence">Machine learning, Leadership tech, Optimisation de modèles, Stratégie produit, parle 4 langues (français, anglais, espagnol, chinoi)</p>
+   <p class="XP">IA, Recherche appliquée, Direction d’équipe, Développement de modèles</p>
+   <p class="passion">Handball, jeux video, l'art culinaire, la musique, le sport en général, chater avec l'ia</p>
+   
+    <footer>
+        © <?php echo date("d/m/Y") ?> - Mon Portfolio
+    </footer>
+</body>
+
+</html>
