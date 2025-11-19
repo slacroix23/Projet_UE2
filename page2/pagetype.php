e4e404e0fff3b92cb8076b0b13d4efbee99cc8b0
Alex04556 <abertrand@guardiaschool.fr>
2025-11-19 16:23:45 +0100

    Update pagetype.php

--- page2/pagetype.php
+++ page2/pagetype.php
@@ -12,36 +12,9 @@
         <h1>Portfolio d'Alexandre</h1>
     </header>
 
-    <main>
-        <form method="post">
-            <label>Email :</label><br>
-            <input type="email" name="email" required><br><br>
-
-            <label>Message :</label><br>
-            <textarea name="message" rows="5" required></textarea><br><br>
-
-            <button type="submit" name="envoyer">Envoyer</button>
-        </form>
-
-
-        <?php
-
-        if (isset($_POST['envoyer'])) {
-            $to = "abertrand@guardiaschool.fr";
-            $subject = "Message depuis le portfolio";
-            $message = $_POST['message'];
-            $headers = "From: " . $_POST['email'];
-
-            if (mail($to, $subject, $message, $headers)) {
-                echo "<p>Merci, votre message a été envoyé !</p>";
-            } else {
-                echo "<p>Erreur lors de l'envoi.</p>";
-            }
-        }
-
-        ?>
-
-</main>
+    <?php
+    
+    ?>
 </body>
 
 </html>
