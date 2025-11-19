0ce2bb0c3b135c91f01d9c9ef755e731b3a41cdc
Alex04556 <abertrand@guardiaschool.fr>
2025-11-19 15:23:14 +0100

    Create pagetype.php

---
+++ page2/pagetype.php
@@ -0,0 +1,20 @@
+<?php
+// Tu peux mettre ici ton code PHP si nécessaire
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="../main/style.css">
+    <title>Portfolio</title>
+</head>
+
+<body>
+    <header class="bandeau">
+        <h1>Portfolio d'Alexandre</h1>
+    </header>
+</body>
+
+</html>
