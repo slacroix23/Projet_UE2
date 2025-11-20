3385619190383c3803d7e32a28cff9f4be16cee8
sorti777 <sorti@guardiaschool.fr>
2025-11-20 16:05:40 +0100

    Update pageSL.php

--- page2/pageSL.php
+++ page2/pageSL.php
@@ -10,7 +10,7 @@
 
 <body>
     <header class="bandeau">
-        <div class="retour">
+        <div>
             <a href="http://localhost/projet_ue2/main/index.html">
                 <img src="../image/flèche_retour.png" alt="bouton_retour" width="30">
             </a>    
