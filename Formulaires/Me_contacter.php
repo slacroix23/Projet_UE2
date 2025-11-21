d240265eea96a5228cb9394f19999a5185c9bc16
Alex04556 <abertrand@guardiaschool.fr>
2025-11-21 12:25:20 +0100

    Update Me_contacter.php

--- Formulaires/Me_contacter.php
+++ Formulaires/Me_contacter.php
@@ -9,6 +9,12 @@
 </head>
 
 <body>
+    <div>
+        <a href="http://localhost/projet_ue2/main/index.html">
+            <img src="../image/flèche_retour.png" alt="bouton_retour" width="30">
+        </a>    
+        </div>
+
     <h1>Contact me</h1>
 
     <?php
