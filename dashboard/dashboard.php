0871fba55d2464e19a3b24c0a8f8a72ecaa9acfb
Syli0on <slacroix@guardiaschool.fr>
2025-12-01 20:15:50 +0100

    start Dashboard

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="dstyle.css">
     <?php
     // Connexion à la BDD (à adapter)
     $pdo = new PDO(
@@ -14,12 +16,17 @@
     $pdo->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
 
     // On récupère les contacts
-    $sql = "SELECT id, nom, email FROM nous ORDER BY nom";
+    $sql = "SELECT * FROM nous ";
+    $sql = "SELECT * FROM login ";
     $stmt = $pdo->query($sql);
     $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
     ?>
 </head>
+
 <body>
-    
+    <header class="bandeau">
+        <h1>Dashboard</h1>
+    </header>
 </body>
+
 </html>
